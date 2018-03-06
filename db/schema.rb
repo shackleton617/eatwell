51ba242810359ba61ed9ce3f1a95f9bace0069d1
francisco gaivao <francisco.abecassis.gaivao@gmail.com>
2018-03-06 14:10:44 +0000

    do migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305174518) do
+ActiveRecord::Schema.define(version: 20180306140911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20180305174518) do
     t.string "photos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "reviews", force: :cascade do |t|
