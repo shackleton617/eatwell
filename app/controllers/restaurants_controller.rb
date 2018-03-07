7b961e28bc9e9c404964bc22a50a931270d77500
Olya Yeremenko <oyeremenko@gmail.com>
2018-03-07 11:18:45 +0000

    pg search working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,34 @@
 class RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-
   before_action :set_restaurant, only: [:show]
 
 
   def index
-    @restaurants = Restaurant.all
+
+    if params[:query].present?
+        sql_query = " \
+        restaurants.name @@ :query \
+        OR restaurants.cuisine @@ :query \
+        OR restaurants.location @@ :query \
+        OR restaurants.address @@ :query \
+        "
+      @restaurants = Restaurant.search_by_any_word(params[:query])
+    else
+      @restaurants = Restaurant.all
+    end
+  end
+
+  def get_location
+    @restaurants = Restaurant.where.not(latitude: nil, longitude: nil)
+
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/restaurants/map_box", locals: { restaurant: restaurant }) }
+      }
+    end
   end
 
   def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,9 +2,17 @@ class Restaurant < ApplicationRecord
   has_many :check_ins
   has_many :users, through: :check_ins
 
-  include AlgoliaSearch
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 
-  algoliasearch do
-    attribute :name, :cuisine, :description, :location
-  end
+  include PgSearch
+  pg_search_scope :search_by_any_word,
+    against: [ :name, :cuisine, :address, :location ],
+    using: {
+      tsearch: { prefix: true, any_word: true }
+
+    }
+    pg_search_scope :search_all_words,
+      against:  [ :name, :cuisine, :address, :location ]
+
 end
