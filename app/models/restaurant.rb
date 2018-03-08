b23ef5a955e7e309c1be62e567ded0b281283391
Mariana Marcelino <mariana_c_marcelino@hotmail.com>
2018-03-08 14:18:43 +0000

    added calculate restaurant average

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,15 +3,15 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :check_ins
   has_many :reviews, dependent: :destroy
 
-
-
+  def average_rating
+    reviews.average(:rating).round(2)
+  end
 
   def calculate_points
     @points = 0
     @points += 10 if self.has_community
     @points += 10 if self.has_recycling
     @points += 10 if self.has_sourcing
-    @points
   end
 
   geocoded_by :address
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,10 @@ class User < ApplicationRecord
   has_many :reviews
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+
+
+
+
+
 end
