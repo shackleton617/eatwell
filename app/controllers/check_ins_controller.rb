8c5993adf09f2aae16e1a2e5cfccb027e95a31c3
Olya Yeremenko <oyeremenko@gmail.com>
2018-03-15 12:34:02 +0000

    pop up when not signed in added

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,16 +1,19 @@
 class CheckInsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @check_in = CheckIn.new
-    @check_in.user = current_user
-    @check_in.restaurant = @restaurant
-    @check_in.save
+    if user_signed_in?
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @check_in = CheckIn.new
+      @check_in.user = current_user
+      @check_in.restaurant = @restaurant
+      @check_in.save
 
-    @points = @restaurant.calculate_points
-    current_user.token_wallet = 0 unless current_user.token_wallet
-    current_user.token_wallet += @points
-    current_user.save
-    # redirect_to dashboard_path
+      @points = @restaurant.calculate_points
+      current_user.token_wallet = 0 unless current_user.token_wallet
+      current_user.token_wallet += @points
+      current_user.save
+    end
+      # redirect_to dashboard_path
 end
 end
