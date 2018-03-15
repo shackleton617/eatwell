d530c6e5b1f542b58a65cb043afa86d2f7aac2cb
Olya Yeremenko <oyeremenko@gmail.com>
2018-03-15 19:54:47 +0000

    added a pop up suggesting on submit a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,23 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new(review_params)
-    @review.restaurant = @restaurant
-    @review.user = current_user
-    @check_in = CheckIn.new
-    if @review.save
-      respond_to do |format|
-        format.html { redirect_to restaurant_path(@restaurant) }
-        format.js  # <-- will render `app/views/reviews/create.js.erb`
-      end
-    else
-      respond_to do |format|
-        format.html { render 'restaurants/show' }
-        format.js  # <-- idem
+    if user_signed_in?
+      @restaurant = Restaurant.find(params[:restaurant_id])
+      @review = Review.new(review_params)
+      @review.restaurant = @restaurant
+      @review.user = current_user
+      @check_in = CheckIn.new
+      if @review.save
+        respond_to do |format|
+          format.html { redirect_to restaurant_path(@restaurant) }
+          format.js  # <-- will render `app/views/reviews/create.js.erb`
+        end
+      else
+        respond_to do |format|
+          format.html { render 'restaurants/show' }
+          format.js  # <-- idem
+        end
       end
     end
   end
