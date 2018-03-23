bdc7c72f59cfc8d17b8b09edf6d9ec01a9c2e461
Daniel Bugge <danielbugge96@gmail.com>
2018-03-23 14:32:40 +0000

    added fake location method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def location
+    if params[:location].blank?
+      if Rails.env.test? || Rails.env.development?
+        @location ||= Geocoder.search("50.78.167.161").first
+      else
+        @location ||= request.location
+      end
+    else
+      params[:location].each {|l| l = l.to_i } if params[:location].is_a? Array
+      @location ||= Geocoder.search(params[:location]).first
+      @location
+    end
+  end
+
   protected
 
    def configure_permitted_parameters
